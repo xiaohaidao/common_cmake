f1e24f14fe1130f2be7d63c9e1ace54ed3f80a88
Li Bobo <libobo@dobot.cc>
2023-09-29 13:24:22 +0800

    Fix error

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -1,10 +1,13 @@
 
-option(GIT_GOOGLETEST_REPOSITORY https://github.com/google/googletest.git)
-option(GOOGLETEST_VERSION release-1.10.0)
+if (GIT_GOOGLETEST_REPOSITORY)
+    set(GIT_GOOGLETEST_REPOSITORY https://github.com/google/googletest.git)
+    set(GOOGLETEST_VERSION release-1.10.0)
+endif()
+
 include(FetchContent)
 FetchContent_Declare(
     googletest
-    GIT_REPOSITORY ${GOOGLETEST_REPOSITORY}
+    GIT_REPOSITORY ${GIT_GOOGLETEST_REPOSITORY}
     GIT_TAG ${GOOGLETEST_VERSION}
     GIT_SHALLOW ON
     GIT_PROGRESS ON
@@ -12,4 +15,3 @@ FetchContent_Declare(
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
-
