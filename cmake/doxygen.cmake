8773564b5aef8b7da29470cd94ee8cff1d9f7e69
Li Bobo <libobo@dobot.cc>
2023-10-08 10:54:21 +0800

    Add doxygen

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,20 @@
+
+find_package(Doxygen OPTIONAL_COMPONENTS dot mscgen dia)
+
+if(DOXYGEN_FOUND AND PROJECT_IS_TOP_LEVEL)
+    # set(DOXYGEN_EXCLUDE_PATTERNS  */build*/* */tests/*)
+    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE "${PROJECT_SOURCE_DIR}/README.md")
+
+    doxygen_add_docs(
+        docs
+        ${PROJECT_SOURCE_DIR}/docs/
+        ${PROJECT_SOURCE_DIR}/src/
+        ${PROJECT_SOURCE_DIR}/include/
+        COMMENT "Generate html pages for ${PROJECT_NAME}"
+    )
+    # install generated files
+    install(
+        DIRECTORY ${PROJECT_BINARY_DIR}/html
+        TYPE DOC
+    )
+endif()
