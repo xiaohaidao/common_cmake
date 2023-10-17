f3d5de9f859e41c868bc5f90e4fc8a2007fc1bc8
Li Bobo <libobo@dobot.cc>
2023-10-17 18:26:32 +0800

    [M] update googletest

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -10,3 +10,35 @@ endif()
 
 #import tool
 include(GNUInstallDirs)
+
+function(RUN_CMD)
+    set(_options)
+    set(_one_arg WORKING_DIRECTORY)
+    set(_multi_arg COMMAND)
+    CMAKE_PARSE_ARGUMENTS(_prefix "${_options}" "${_one_arg}" "${_multi_arg}" ${ARGN})
+    set(_unuse_list ${_prefix_UNPARSED_ARGUMENTS})
+    set(_cmd ${_prefix_COMMAND})
+    set(_dir ${_prefix_WORKING_DIRECTORY})
+
+    #message("run command : ${_cmd}")
+    if(_dir)
+        message("WORKING_DIRECTORY : ${_dir}")
+        execute_process(
+            COMMAND ${_cmd}
+            RESULT_VARIABLE err_result
+            WORKING_DIRECTORY ${_dir}
+            COMMAND_ECHO STDOUT
+        )
+    else()
+        execute_process(
+            COMMAND ${_cmd}
+            RESULT_VARIABLE err_result
+            COMMAND_ECHO STDOUT
+        )
+    endif()
+    if(err_result)
+        message(FATAL_ERROR "execute process error ${err_result}")
+    endif()
+
+endfunction()
+
--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -1,9 +1,17 @@
 
+##
+# @brief googletest
+# @GIT_GOOGLETEST_REPOSITORY
+# @GOOGLETEST_VERSION
+# @GTEST_INSTALL_DIR
+# link GTest:gtest
+# link GTest:gtest_main
+#
 if (NOT GIT_GOOGLETEST_REPOSITORY)
     set(GIT_GOOGLETEST_REPOSITORY https://github.com/google/googletest.git)
 endif()
 if (NOT GOOGLETEST_VERSION)
-    set(GOOGLETEST_VERSION release-1.10.0)
+    set(GOOGLETEST_VERSION release-1.10.0) # c++11
 endif()
 
 include(FetchContent)
@@ -21,20 +29,26 @@ FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
     FetchContent_Populate(googletest)
     set(target_dir ${googletest_BINARY_DIR}/build/)
-    execute_process(
+    set(src_dir ${googletest_SOURCE_DIR}/)
+    if(NOT GTEST_INSTALL_DIR)
+        set(install_dir ${target_dir}/install)
+    else()
+        set(install_dir ${GTEST_INSTALL_DIR})
+    endif()
+
+    RUN_CMD(
         COMMAND ${CMAKE_COMMAND} -B ${target_dir} . -DCMAKE_BUILD_TYPE=Release
-        WORKING_DIRECTORY ${googletest_SOURCE_DIR}
-        # COMMAND_ECHO STDOUT
+        WORKING_DIRECTORY ${src_dir}
     )
-    execute_process(
+    RUN_CMD(
         COMMAND ${CMAKE_COMMAND} --build  ${target_dir} -j
-        WORKING_DIRECTORY ${googletest_SOURCE_DIR}
+        WORKING_DIRECTORY ${src_dir}
     )
-    execute_process(
-        COMMAND ${CMAKE_COMMAND} --install ${target_dir} --prefix  ${target_dir}/install
-        WORKING_DIRECTORY ${googletest_SOURCE_DIR}
+    RUN_CMD(
+        COMMAND ${CMAKE_COMMAND} --install ${target_dir} --prefix  ${install_dir}
+        WORKING_DIRECTORY ${src_dir}
     )
-    set(GTest_ROOT ${target_dir}/install/)
-    set(GTest_DIR ${target_dir}/install/)
+    set(GTest_ROOT ${install_dir})
+    set(GTest_DIR ${install_dir})
     find_package(GTest REQUIRED)
 endif()
