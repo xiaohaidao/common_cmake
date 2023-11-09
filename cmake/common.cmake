e421b511272e42f020e5a057255082721d95c455
Li Bobo <libobo@dobot.cc>
2023-11-09 09:50:57 +0800

    [A] Add FIND_OR_BUILD function

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -11,6 +11,7 @@ endif()
 #import tool
 include(GNUInstallDirs)
 
+## following is RUN_CMD
 function(RUN_CMD)
     set(_options)
     set(_one_arg WORKING_DIRECTORY)
@@ -42,3 +43,77 @@ function(RUN_CMD)
 
 endfunction()
 
+## following is FIND_OR_BUILD
+if(WIN32)
+    set(HOME $ENV{USERPROFILE})
+    string(REPLACE "\\" "/" HOME ${HOME})
+else()
+    set(HOME $ENV{HOME})
+endif()
+string(TOLOWER ${CMAKE_SYSTEM_PROCESSOR}-${CMAKE_SYSTEM_NAME}-${CMAKE_C_COMPILER_ID} local_install)
+set(local_install ${HOME}/.cpp/${local_install})
+set(local_install_dev ${HOME}/.cpp/${local_install}-dev)
+message("local_install ${local_install}")
+
+macro(FIND_OR_BUILD)
+    set(_options)
+    set(_one_arg TARGET GIT_URL GIT_TAGS INSTALL_DIR)
+    set(_multi_arg CMAKE_APPEND)
+    CMAKE_PARSE_ARGUMENTS(_prefix "${_options}" "${_one_arg}" "${_multi_arg}" ${ARGN})
+    set(_unuse_list ${_prefix_UNPARSED_ARGUMENTS})
+    set(_target ${_prefix_TARGET})
+    set(_git_url ${_prefix_GIT_URL})
+    set(_git_tags ${_prefix_GIT_TAGS})
+    set(_install_dir ${_prefix_INSTALL_DIR})
+    set(_cmake_append ${_prefix_CMAKE_APPEND})
+
+    if(NOT ${_target}_FOUND)
+        set(install_dir ${_install_dir}/${_target}-${_git_tags}/)
+        find_package(${_target} CONFIG PATHS ${install_dir} NO_DEFAULT_PATH)
+        if (${_target}_FOUND)
+            message("${_target}_CONFIG path : ${${_target}_CONFIG}")
+        endif()
+
+    endif()
+
+    # if(${CMAKE_VERSION} VERSION_LESS "3.16")
+    #     set(empty_submodule "src")
+    # else()
+    #     cmake_policy(SET CMP0097 NEW)
+    #     set(empty_submodule "")
+    # endif()
+    FetchContent_Declare(
+        ${_target}
+        GIT_REPOSITORY ${_git_url}
+        GIT_TAG ${_git_tags}
+        # GIT_SUBMODULES "src"
+        GIT_SHALLOW ON
+        GIT_PROGRESS ON
+    )
+    FetchContent_GetProperties(${_target})
+    if(NOT (${_target}_POPULATED OR ${_target}_FOUND))
+        FetchContent_Populate(${_target})
+        string(TOLOWER ${_target} _target_lower)
+        set(target_dir ${${_target_lower}_BINARY_DIR}/build/)
+        set(src_dir ${${_target_lower}_SOURCE_DIR}/)
+
+        message("_cmake_append ${_cmake_append}")
+        RUN_CMD(
+            COMMAND ${CMAKE_COMMAND} -B ${target_dir} . -DCMAKE_INSTALL_PREFIX=${install_dir} ${_cmake_append}
+            WORKING_DIRECTORY ${src_dir}
+        )
+        RUN_CMD(
+            COMMAND ${CMAKE_COMMAND} --build ${target_dir} -j
+            WORKING_DIRECTORY ${src_dir}
+        )
+        RUN_CMD(
+            # COMMAND ${CMAKE_COMMAND} --install ${target_dir} --prefix  ${install_dir}
+            COMMAND ${CMAKE_COMMAND} --build ${target_dir} -j --target install
+            WORKING_DIRECTORY ${src_dir}
+        )
+
+        find_package(${_target} CONFIG REQUIRED PATHS ${install_dir} NO_DEFAULT_PATH)
+        message("${_target}_CONFIG build path : ${${_target}_CONFIG}")
+    endif()
+
+endmacro()
---
+++ cmake/dep_gtest.cmake
@@ -0,0 +1,29 @@
+
+##
+# @brief GTest
+# @GIT_GTEST_REPOSITORY
+# @GTEST_VERSION
+# @GTEST_INSTALL_DIR
+# link GTest:gtest
+# link GTest:gtest_main
+#
+if (NOT GIT_GTEST_REPOSITORY)
+    set(GIT_GTEST_REPOSITORY https://github.com/google/googletest.git)
+endif()
+if (NOT GTEST_VERSION)
+    set(GTEST_VERSION release-1.10.0) # c++11
+endif()
+if (NOT GTEST_INSTALL_DIR)
+    set(GTEST_INSTALL_DIR ${local_install})
+endif()
+
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+#set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+FIND_OR_BUILD(
+    TARGET GTest
+    GIT_URL ${GIT_GTEST_REPOSITORY}
+    GIT_TAGS ${GTEST_VERSION}
+    INSTALL_DIR ${GTEST_INSTALL_DIR}
+    CMAKE_APPEND -DCMAKE_BUILD_TYPE=Release -Dgtest_force_shared_crt=ON
+)
