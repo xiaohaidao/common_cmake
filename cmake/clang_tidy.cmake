b1a7d1e258bf46236e52e822f159f3516a1906fe
Li Bobo <libobo@dobot.cc>
2024-02-01 18:03:58 +0800

    [M] fix clang_tidy cmake

--- cmake/clang_tidy.cmake
+++ cmake/clang_tidy.cmake
@@ -1,12 +1,13 @@
 
 find_program(CLANG_TIDY_TOOL clang-tidy)
 if (PROJECT_IS_TOP_LEVEL AND CLANG_TIDY_TOOL)
+set(CLANG_TIDY_TOOL "${CLANG_TIDY_TOOL}\;--fix\;--fix-errors")
 add_custom_target(clang_tidy
     COMMAND
         ${CMAKE_COMMAND}
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-        -DCMAKE_C_CLANG_TIDY="${CLANG_TIDY_TOOL};--fix-errors"
-        -DCMAKE_CXX_CLANG_TIDY="${CLANG_TIDY_TOOL};--fix-errors"
+        -DCMAKE_C_CLANG_TIDY="${CLANG_TIDY_TOOL}"
+        -DCMAKE_CXX_CLANG_TIDY="${CLANG_TIDY_TOOL}"
         -S${PROJECT_SOURCE_DIR} -B${PROJECT_BINARY_DIR}
     COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR} -j
     COMMENT "Check code with Clang-Tidy"
